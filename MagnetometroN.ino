cc7bd28ecd0ba7166202664b8646a6df30dd9a51
AndreaColonna <96191719+AndreaColonna@users.noreply.github.com>
2023-02-25 18:49:19 +0100

    Update MagnetometroN.ino

--- MagnetometroN.ino
+++ MagnetometroN.ino
@@ -1,23 +1,18 @@
 /*
-  Arduino LSM9DS1 - Misura del campo magnetico 
-
-  In questo esempio utilizziamo il sensore presente in nano 33 ble sense
-  per misurare il campo ambientale e sottrarlo a quello prodotto da un filo percorso da corrente
-
-  The circuit:
-  - Arduino Nano 33 BLE Sense
-Andrea Colonna
-
-  This example code is in the public domain.
+ Misura le componenti del campo mg terrestre in un punto
+ B_x, B_y, B_z calcola il valor medio in 5 sec
+ Calcola il valore del campo magnetico sottraendogli il campo magnetico terrestre 
 */
 
 #include <Arduino_LSM9DS1.h>
-float B_x=0.0;
-float B_y=0.0;
-float B_z=0.0;
-int N = 0;
+float B_x=0.0; /*variabile per memorizzare il campo magnetico medio lungo x*/ 
+float B_y=0.0; /*variabile per memorizzare il campo magnetico medio lungo y*/
+float B_z=0.0; /*variabile per memorizzare il campo magnetico medio lungo z*/
+float B = 0.0; /*modulo del campo magnetico*/
+int N = 0;     /*Numero di campioni*/
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(9600); 
+  
   while (!Serial);
   Serial.println("Started");
 
@@ -25,39 +20,45 @@ void setup() {
     Serial.println("Failed to initialize IMU!");
     while (1);
   }
+  
   Serial.print("Magnetic field sample rate = ");
+  
   Serial.print(IMU.magneticFieldSampleRate());
+  
   Serial.println(" Hz");
   Serial.println();
   Serial.println("Magnetic Field in uT");
-  Serial.println("X\tY\tZ");
-  float ti = millis();
-  /*
-  Procedura per calcolare il valor medio del campo magnetico di un luogo
-  */
+  Serial.println("X-BX\tY-BY\tZ-Bz\tB");
   
+  float ti = millis(); /*tempo iniziale in millisecondi*/
+  float x, y, z;      /*variabili in cui sono memorizzate le componenti del campo magnetico*/
   while((millis()-ti)<5000){
-    IMU.readMagneticField(B_x, B_y, B_z);
-    B_x+=B_x;
-    B_y+=B_y;
-    B_z+=B_z;
-    N+=1;
+    IMU.readMagneticField(x, y, z); /*lettura delle componenti del campo magnetico*/
+    B_x+=x;                         /*somma i valori del campo magnetico*/
+    B_y+=y;
+    B_z+=z;
+    N+=1;                           /*incrementa il numero di valori sommati*/
     }
-    B_x=B_x/N;
+    B_x=B_x/N;                      /*calcola il campo magnetico medio*/
     B_y=B_y/N;
     B_z=B_z/N;
 }
 
 void loop() {
-  float x, y, z;
+  float x, y, z;            /*variabili in cui sono memorizzate le componenti del campo magnetico*/   
 
   if (IMU.magneticFieldAvailable()) {
-    IMU.readMagneticField(x, y, z);
-
-    Serial.print(x-B_x);
+    IMU.readMagneticField(x, y, z);  /*lettura delle componenti del campo magnetico*/
+    x=x-B_x;                         
+    y=y-B_y;
+    z=z-B_z;
+    B=sqrt(x*x+y*y+z*z);
+    Serial.print(x);
     Serial.print('\t');
-    Serial.print(y-B_y);
+    Serial.print(y);
     Serial.print('\t');
-    Serial.println(z-B_z);
+    Serial.println(z);
+    Serial.print('\t');
+    Serial.print(B);
   }
 }
